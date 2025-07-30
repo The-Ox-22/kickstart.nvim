1982625d1f54ddfe367583a1037e4c4818dd2b9f
The-Ox-22 <iwf777@hotmail.com>
2025-07-30 11:57:20 -0500

    use a working sops plugin

--- init.lua
+++ init.lua
@@ -402,12 +402,23 @@ require('lazy').setup({
     },
   },
 
+  {
+    'lucidph3nx/nvim-sops',
+    event = { 'BufEnter' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      { '<leader>ef', vim.cmd.SopsEncrypt, desc = '[E]ncrypt [F]ile' },
+      { '<leader>df', vim.cmd.SopsDecrypt, desc = '[D]ecrypt [F]ile' },
+    },
+  },
+  -- Doesn't work. Cant save/re encrypt files for some reason
   -- {
   --   'trixnz/sops.nvim',
   --   lazy = false,
-  --   config = function()
-  --     require('sops.nvim').setup {}
-  --   end,
   -- },
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
@@ -1050,7 +1061,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
 
